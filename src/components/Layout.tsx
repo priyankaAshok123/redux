7baec7582b7180b0d2fc0110c1d26491657bf68e
Priyanka <priyankaashok@giveindia.org>
2026-06-14 12:09:26 +0530

    Removes code

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,8 @@
-import React from 'react'
 import { Outlet } from 'react-router-dom'
-import Header from './Header'
 
 const Layout = () => {
     return (
         <div>
-            {/* <Header /> */}
             <main>
                 <Outlet />
             </main>
