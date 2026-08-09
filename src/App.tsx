4a0c08b2a106dcaa47eace1a7e3a278bc4ec31ee
Priyanka <priyankaashok@giveindia.org>
2026-08-10 02:35:33 +0530

    docker setup to run locally

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 
-import { useState } from 'react';
 import './App.css';
-import Cart from './components/Cart/Cart';
 
 function App() {
-  const [cartItems, setCartItems] = useState([]);
   return (
     <div>
-      <Cart cartItems={cartItems} setCartItems={setCartItems}/>
+     Hello....
     </div>
   );
 }
--- src/components/Cart/ProductOffCanvas.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ProductOffCanvas = () => {
-  return (
-    <div>ProductOffCanvas</div>
-  )
-}
-
-export default ProductOffCanvas
